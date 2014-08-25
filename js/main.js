e664e1deecca17b035418b72ac69a4802776a061
takuyakanbr <takuyakanbr@gmail.com>
2014-08-25 20:50:57 +0800

    Hide dialog on esc key Misc CSS changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
-WEBR = {};
+WEBR = {
+    nwgui: require('nw.gui'),
+    util: require('utils'),
+    db: require('db'),
+    cheerio: require('cheerio'),
+    moment: require('moment')
+};
 WEBR.Dialog = (function () {
     var $overlay = $('.webr-overlay'),
         isActive = false,
         activeDialog = null;
+    
+    function hide() {
+        if (isActive) {
+            activeDialog.fadeOut(400);
+            $overlay.fadeOut(400);
+            activeDialog = null;
+            isActive = false;
+        }
+    }
+    
     $('.webr-ovd').hide();
+    $(document).keyup(function (e) {
+        if (e.keyCode == 27) { // 'ESC' key
+            hide();
+        }
+    })
     
     return {
         show: function (dialogName) {
@@ -11,81 +32,17 @@ WEBR.Dialog = (function () {
                 activeDialog.hide();
             }
             isActive = true;
-            $overlay.show();
+            $overlay.fadeIn(400);
             activeDialog = $('#webr-ovd-' + dialogName);
             activeDialog.fadeIn(400);
         },
-        hide: function () {
-            if (isActive) {
-                activeDialog.fadeOut(400);
-                $overlay.hide();
-                activeDialog = null;
-            }
-        }
+        hide: hide
     };
 }());
-
-
-(function () {
-    'use strict';
-    
-    var nwgui = require('nw.gui'),
-        util = require('utils'),
-        db = require('db'),
-        cheerio = require('cheerio'),
-        moment = require('moment');
-    
-    var selectedFeed = -1,
-        wid = window.innerWidth,
-        hgt = window.innerHeight;
-    
-    
-    function setupHandlers() {
-        var heightChangeCallback = function() {
-            var width = window.innerWidth,
-                height = window.innerHeight;
-            if (width != wid || height != hgt) {
-                setTimeout(doResize, 500);
-                wid = width;
-                hgt = height;
-            }
-        };
-        doResize();
-        setInterval(db.pullAllFeeds, 60000);
-        window.onresize = heightChangeCallback;
-        
-        // clicked feed list item
-        $('.webr-feedlist').on('click', '.webr-feeditem a', function (e) {
-            e.preventDefault();
-            $('.webr-feeditem').removeClass('webr-feeditem-selected');
-            $(this).parent().addClass('webr-feeditem-selected');
-            selectedFeed = $(this).data('id');
-            renderArticleList(selectedFeed);
-            renderArticle(null, null);
-        });
-        
-        // clicked article list item
-        $('.webr-articlelisttable').on('click', '.webr-articleitem', function (e) {
-            e.preventDefault();
-            var fid = $(this).data('fid'),
-                link = $(this).data('link');
-            $('.webr-articleitem').removeClass('webr-articleitem-selected');
-            $(this).addClass('webr-articleitem-selected');
-            renderArticle(fid, link);
-        });
-        
-        $('#webr-ct-add').click(function () {
-            WEBR.Dialog.show('addfeed');
-        });
-    }
-    
-    function doResize() {
-        var sidebarWid = wid * 0.3;
-        if (sidebarWid > 300) sidebarWid = 300;
-        $('.webr-sidebar').css({width: sidebarWid});
-        $('.webr-mainpage').css({width: wid - sidebarWid - 2, left: sidebarWid + 2});
-        $('.webr-articlelist').css({width: wid - sidebarWid - 2});
-    }
+WEBR.Display = (function() {
+    var db = WEBR.db,
+        util = WEBR.util,
+        moment = WEBR.moment;
     
     // renders an article given feed ID and article link
     function renderArticle(fid, link) {
@@ -142,8 +99,8 @@ WEBR.Dialog = (function () {
         if (cont && cont.length > 0) {
             for (var i in cont) {
                 var $tr = $('<tr class="webr-articleitem"></tr>').data('link', cont[i].link).data('fid', fid);
-                $('<td class="webr-article-title"></td>').html(cont[i].title).appendTo($tr);
-                $('<td class="webr-article-date"></td>').html(moment(cont[i].date).format('DD MMM YYYY hh:mm:ss a')).appendTo($tr);
+                $('<td class="webr-articleitem-title"></td>').html(cont[i].title).appendTo($tr);
+                $('<td class="webr-articleitem-date"></td>').html(moment(cont[i].date).format('DD MMM YYYY hh:mm:ss a')).appendTo($tr);
                 $tr.appendTo($articlelist);
             }
         } else {
@@ -155,7 +112,6 @@ WEBR.Dialog = (function () {
     function renderFeedList() {
         var $feedlist = $('.webr-feedlist');
         $feedlist.html('');
-        console.log(db.feeds);
         if (db.feeds.length == 0) {
             $('<div class="webr-feeditem></div>').html('No feeds added.').appendTo($feedlist);
         } else {
@@ -165,9 +121,78 @@ WEBR.Dialog = (function () {
         }
     }
     
+    return {
+        renderArticle: renderArticle,
+        renderArticleList: renderArticleList,
+        renderFeedList: renderFeedList
+    };
+}());
+
+
+(function () {
+    'use strict';
+    
+    var nwgui = WEBR.nwgui,
+        util = WEBR.util,
+        db = WEBR.db,
+        cheerio = WEBR.cheerio,
+        moment = WEBR.moment;
+    
+    var selectedFeed = -1,
+        wid = window.innerWidth,
+        hgt = window.innerHeight;
+    
+    
+    function setupHandlers() {
+        var heightChangeCallback = function() {
+            var width = window.innerWidth,
+                height = window.innerHeight;
+            if (width != wid || height != hgt) {
+                setTimeout(doResize, 500);
+                wid = width;
+                hgt = height;
+            }
+        };
+        doResize();
+        setInterval(db.pullAllFeeds, 60000);
+        window.onresize = heightChangeCallback;
+        
+        // clicked feed list item
+        $('.webr-feedlist').on('click', '.webr-feeditem a', function (e) {
+            e.preventDefault();
+            $('.webr-feeditem').removeClass('webr-feeditem-selected');
+            $(this).parent().addClass('webr-feeditem-selected');
+            selectedFeed = $(this).data('id');
+            WEBR.Display.renderArticleList(selectedFeed);
+            WEBR.Display.renderArticle(null, null);
+        });
+        
+        // clicked article list item
+        $('.webr-articlelisttable').on('click', '.webr-articleitem', function (e) {
+            e.preventDefault();
+            var fid = $(this).data('fid'),
+                link = $(this).data('link');
+            $('.webr-articleitem').removeClass('webr-articleitem-selected');
+            $(this).addClass('webr-articleitem-selected');
+            WEBR.Display.renderArticle(fid, link);
+        });
+        
+        $('#webr-ct-add').click(function () {
+            WEBR.Dialog.show('addfeed');
+        });
+    }
+    
+    function doResize() {
+        var sidebarWid = wid * 0.3;
+        if (sidebarWid > 300) sidebarWid = 300;
+        $('.webr-sidebar').css({width: sidebarWid});
+        $('.webr-mainpage').css({width: wid - sidebarWid - 2, left: sidebarWid + 2});
+        $('.webr-articlelist').css({width: wid - sidebarWid - 2});
+    }
+    
     setupHandlers();
     db.loadData(nwgui, function () {
-        renderFeedList();
+        WEBR.Display.renderFeedList();
     });
     
 }());
